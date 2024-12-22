c6f34946fbb95eb06df02b70ff154e41346dccca
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-12-22 15:59:11 -0300

    chore: build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,27 @@
+# install npm dependencies and setup rust or whatever is needed
+npm install
+
+# build apps
 cargo build --release
+npm run build
 
 # make important directories
-mkdir package
-mkdir package/www
+mkdir -p package/www
 
 # copy important things into it
 cp ./target/release/deploy-experiment ./package/
-cp -a ./public ./package
-cp ./package* ./package/
+if [ -d "public" ]; then
+    cp -a public package
+fi
+
+if [ -d "dist" ]; then
+    cp -a dist package
+fi
+
+cp package-lock.json ./package/
+cp package.json ./package/
 cp ./www/root.html ./package/www/
 cp ./run.sh ./package/
-cp ./squarecloud.app ./package/
 
 # create a fake ts file so that squarecloud recognizes nodejs
 echo "console.log('run')" > ./package/main.ts
